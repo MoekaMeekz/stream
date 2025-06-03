08b44f0277fe065c20a60784703a1243d860924d
Moeka Meekz <95100987+MoekaMeekz@users.noreply.github.com>
2025-06-03 23:17:12 +0100

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const io = new Server(server);
 const mongoose = require("mongoose");
 const User = require("./models/User");
 
-mongoose.connect(processs.env.MONGO_URI, {
+mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
